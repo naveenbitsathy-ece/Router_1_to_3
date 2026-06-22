3dee620f0f4e22046eb49cc524a649e8f4bcc4af
Naveen Raj <your_github_email@example.com>
2026-06-23 00:27:28 +0530

    FIFO need to verify

---
+++ FIFO.v
@@ -0,0 +1,83 @@
+`timescale 1ns/1ps
+module FIFO(
+    input clk,rst_n,
+    input soft_rst,
+    input wr_en,rd_en,
+    input lfd_state,
+    input data_in[8:0],
+    output reg data_out[8:0],
+    output full,empty
+);
+
+reg [8:0]FIFO[15:0];  // Depth 16 bit and 9 bit width
+reg [3:0]wr_ptr;
+reg [3:0]rd_ptr;
+reg [3:0]counter;    // count 0 to 16
+integer i;
+
+always@(posedge clk or negedge rst_n)
+begin
+    if(rst_n)
+    data_out<=8'b0;
+     for(i=0;i<16;i=i+1)      // Whole FIFO is 0 and data_out is 0
+     mem[i]<=0; 
+    counter[i]<=0;
+
+    if(soft_rst)
+    data_out<=8'b0;
+    for(i=0;i<16;i=i+1)       // Whole FIFO is 0 and data_out is 0
+    mem[i]<=0; 
+    counter[i]<=0;
+    
+end
+
+always@(posedge clk)
+begin 
+if(wr_en && !full)
+     counter<=counter+1'b1;    // if write happens counter increments 
+
+if(rd_en && !empty)
+    counter<=counter-1'b1;     // if read happens counter decrements 
+
+if((wr_en && !full) && (rd_en && !empty))
+    counter<=counter;      // If read and write happens counter stays same 
+
+else
+    counter<=counter;      // Condition to prevent latch formation
+end 
+
+// condition for full and empty
+always@(*)
+begin
+if (counter==4'd16)  
+    full=1'b1;               // writes data from 0 to 15 totally 16 so Full
+if (counter==4'd0)
+    empty=1'b1;              //If counter is zero then no write happens so empty
+end 
+
+//Data write and data read 
+always@(posedge clk)
+begin 
+if(wr_en && !full)
+ FIFO[wr_ptr]<=data_in;
+ wr_ptr<=wr_ptr+1'b1;
+
+ if(rd_en && !empty)
+ data_out <= FIFO[rd_ptr];
+ rd_ptr<=rd_ptr+1'b1;
+
+ if((wr_en && !full) && (rd_en && !empty))
+ begin
+     FIFO[wr_ptr]<=data_in;
+     data_out <= FIFO[rd_ptr];       
+     rd_ptr<=rd_ptr;                // If write and read happens together then
+     wr_ptr<=wr_ptr;                 // read pointer and write pointer stays at same 
+ end 
+end 
+
+//  if(data_in[8]==1'b1)
+//      Header;
+//  else 
+//  Not Header;
+
+ endmodule 
---
+++ FIFO_tb.v
@@ -0,0 +1,83 @@
+module FIFO_tb();
+    reg clk,rst_n;
+    reg soft_rst;
+    reg wr_en,rd_en;
+    reg lfd_state;
+    reg data_in[8:0];
+    wire data_out[8:0];
+    wire full,empty;
+
+reg [8:0]FIFO[15:0];  // Depth 16 bit and 9 bit width
+reg wr_ptr[3:0];
+reg rd_ptr[3:0];
+reg counter[3:0];    // count 0 to 16
+
+FIFO DUT(
+     clk,rst_n,
+     soft_rst,
+     wr_en,rd_en,
+     lfd_state,
+     data_in,
+     data_out,
+     full,empty
+);
+initial 
+clk=0;
+always 
+#5
+clk=~clk;
+
+initial 
+begin
+   
+    rst_n=0;
+    #10;
+    rst_n=1;
+#5;
+   soft_rst=1;
+   #10;
+   soft_rst=0;
+end 
+
+task stimula(
+    input a,b,
+    input [8:0]c);
+    begin
+        a=wr_en;
+        b=rd_en;
+        c=data_in; 
+        #10;
+    end 
+endtask;
+
+initial 
+begin
+    stimula(1,0,9'd4);
+    stimula(1,0,9'd4);   //Write 
+    stimula(1,0,9'd4);
+    stimula(1,0,9'd4); 
+
+
+    stimula(0,1,9'd4);
+    stimula(0,1,9'd5);   //read
+    stimula(0,1,9'd4);
+    stimula(0,1,9'd5); 
+
+    stimula(1,1,9'd4);
+    stimula(1,1,9'd5);   // write and read 
+
+    #200;
+$finish ;
+end 
+initial
+begin
+    $dumpfile("FIFO.vcd");
+    $dumpvars(0,FIFO_tb.v); 
+end 
+
+initial 
+begin
+    $monitor("Time=$0t rst_n=%b soft_rst=%b wr_en=%b rd_en=%b full=%b empty=%b data_in=%b data_out=%b",
+    $time,rst_n,soft_rst,wr_en.rd_en,full,empty,data_in,data_out); 
+end 
+endmodule
