33e348db0ff543f89d4ed905036089d1f967d3c6
Naveen Raj <your_github_email@example.com>
2026-06-23 11:56:33 +0530

    Corrected

--- Internal_counter_fifo.v
+++ Internal_counter_fifo.v
@@ -4,28 +4,28 @@ module Internal_counter_fifo(
     input [7:0]data_in,
     input lfd_state,
     output reg [7:0]data_out,
-    input empty,full
+    output empty,full
 );
 reg [8:0]mem[0:15];
  reg [6:0]counter;
  reg [4:0]rd_ptr,wr_ptr;
-
+integer i;
 //--------------------------------------------------------------------------------------//s
 // Payload length count logic 
 always@(posedge clk)
-if(rst_in)
+if(rst_n)
     counter<=7'b0;
 else if(soft_rst)
 counter<=7'b0;
-else if(read_en && (!empty))   // empty has high priority it will check first 
- if(mem[rd_ptr[3:0]][8]==1'b1)
-count <= mem[rd_ptr[3:0]][7:2];
-else if(count != 0)
+else if(rd_en && !empty)  
+     if(mem[rd_ptr[3:0]][8]==1'b1)
+counter <= mem[rd_ptr[3:0]][7:2];
+     else if(counter != 0)
      counter<=counter-1;
 //--------------------------------------------------------------------------------------//
 
 assign full=(wr_ptr  == {!rd_ptr[4],rd_ptr[3:0]});   //Full logic 
-assign empty=(ed_ptr==wr_ptr);                       //empty logic 
+assign empty=(rd_ptr==wr_ptr);                       //empty logic 
 
 //--------------------------------------------------------------------------------------//
 //Read logic 
@@ -44,7 +44,7 @@ begin
     end 
     else if(counter==0 && data_out!=0 )
     data_out<=8'hz;
-    else if(read_en && !empty)
+    else if(rd_en && !empty)
     begin
         data_out<=mem[rd_ptr[3:0]];
         rd_ptr<=rd_ptr+1;
@@ -67,7 +67,7 @@ begin
             for(i=0;i<16;i=i+1)
         mem[i] <= 0; 
     end 
-    else if (write_en && !full)
+    else if (wr_en && !full)
     begin 
     mem[wr_ptr[3:0]] <= {lfd_state,data_in};
     wr_ptr<=wr_ptr+1;
