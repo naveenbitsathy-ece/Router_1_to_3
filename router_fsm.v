7b46381f04d2805e0856216fc5afd63240056809
Naveen Raj <your_github_email@example.com>
2026-06-24 15:26:50 +0530

    RTL completed

--- router_fsm.v
+++ router_fsm.v
@@ -1,18 +1,18 @@
-`timescale 1ns/1ps
+
 module router_fsm(
   input clk,rst_n,
   input pkt_valid,
   input parity_done,
-  input [1:0]data_in;
+  input [1:0]data_in,
   input soft_rst_0,
   input soft_rst_1,
   input soft_rst_2,
   input fifo_full,
-  input low_pkt_valid,
   input fifo_empty_0,
   input fifo_empty_1,
   input fifo_empty_2,
-
+  
+  output low_pkt_valid,
   output detect_add,
   output busy,
   output ld_state,
@@ -22,27 +22,28 @@ module router_fsm(
   output rst_int_reg,
   output lfd_state
 );
-parameter 3'b000 = DECODE_ADDRESS ;
-parameter 3'b001 = LOAD_FIRST_DATA;
-parameter 3'b010 = LOAD_DATA;
-parameter 3'b011 = WAIT_TILL_EMPTY;
-parameter 3'b100 = FIFO_FULL_STATE;
-parameter 3'b101 = LOAD_PARITY;
-parameter 3'b110 = CHECK_PARITY_ERROR;
-parameter 3'b111 = LOAD_AFTER_FULL;
+parameter  DECODE_ADDRESS=3'b000 ; 
+parameter  LOAD_FIRST_DATA = 3'b001 ;
+parameter  LOAD_DATA = 3'b010 ;
+parameter  WAIT_TILL_EMPTY = 3'b101 ;
+parameter  FIFO_FULL_STATE = 3'b100;
+parameter  LOAD_PARITY = 3'b101;
+parameter  CHECK_PARITY_ERROR = 3'b110;
+parameter  LOAD_AFTER_FULL = 3'b111;
 
 
 reg [2:0] state,next_state;
-reg [1:0] addr;
+reg [1:0] addr;          //Temp reg to store address 
+
+
 //Present state sequential logic  -- Middle block 
 
 always@(posedge clk)
 begin 
   if(!rst_n)
-  begin
-    
-    state<=DECODE_ADDRESS;
-  end 
+    state <= DECODE_ADDRESS;
+  else if(soft_rst_0 || soft_rst_1 || soft_rst_2)
+    state <= DECODE_ADDRESS;
   else
     state<=next_state;
 end
@@ -50,21 +51,22 @@ end
 // Next state combinational logic 
 always@(*)
 begin
+addr=data_in; 
  next_state = DECODE_ADDRESS;
  case(state)
  DECODE_ADDRESS:begin            //DECODE_ADDRESS
 
-  addr=data_in; 
+  
 
-  if((pkt_valid & (data_in[1:0] = 0) & fifo_empty_0)|
-  (pkt_valid & (data_in[1:0] = 0) & fifo_empty_1)|
-  (pkt_valid & (data_in[1:0] = 0) & fifo_empty_1) )
-   
+  if((pkt_valid && data_in[1:0]==2'b00 && fifo_empty_0) ||
+   (pkt_valid && data_in[1:0]==2'b01 && fifo_empty_1) ||
+   (pkt_valid && data_in[1:0]==2'b10 && fifo_empty_2))
+	
        next_state=LOAD_FIRST_DATA;
 
-  else if((pkt_valid & (data_in[1:0] = 0) & !fifo_empty_0)|
-  (pkt_valid & (data_in[1:0] = 0) & !fifo_empty_1)|
-  (pkt_valid & (data_in[1:0] = 0) & !fifo_empty_1) )
+  else if((pkt_valid & data_in[1:0] == 2'b00 & !fifo_empty_0)||
+  (pkt_valid & data_in[1:0] == 2'b01 & !fifo_empty_1)||
+  (pkt_valid & data_in[1:0] == 2'b10 & !fifo_empty_2) )
 
        next_state = WAIT_TILL_EMPTY;
 
@@ -72,7 +74,7 @@ begin
       next_state = DECODE_ADDRESS;     
  end 
 
- LOAD_FIRST_DATA:begin 
+ LOAD_FIRST_DATA:begin             //LOAD_FIRST_DATA
   next_state = LOAD_DATA;
  end 
 
@@ -87,7 +89,7 @@ begin
  end 
 
  WAIT_TILL_EMPTY:begin            // WAIT_TILL_EMPTY
-  if((fifo_empty_0 && (addr == 0)) || (fifo_empty_0 && (addr == 1))
+  if((fifo_empty_0 && (addr == 0)) || (fifo_empty_0 && (addr == 1)) ||
   (fifo_empty_0 && (addr == 2)))
 
        next_state = LOAD_FIRST_DATA;
@@ -96,7 +98,7 @@ begin
  next_state = WAIT_TILL_EMPTY;
  end 
 
- FIFO_FULL_STATE:begin               //FIFO_FULL_STATE
+ FIFO_FULL_STATE:begin                //FIFO_FULL_STATE
   if(fifo_full)
   next_state = FIFO_FULL_STATE;
   else  // !fifo_full
@@ -106,7 +108,7 @@ begin
  LOAD_PARITY: next_state = CHECK_PARITY_ERROR ;     //LOAD_PARITY
  
 
- CHECK_PARITY_ERROR:begin             // CHECK_PARITY_ERROR
+ CHECK_PARITY_ERROR:begin                // CHECK_PARITY_ERROR
   if(!fifo_full)
   next_state = DECODE_ADDRESS;
   else 
@@ -114,20 +116,38 @@ begin
 
  end 
 
- LOAD_AFTER_FULL:begin            //LOAD_AFTER_FULL
+ LOAD_AFTER_FULL:begin               //LOAD_AFTER_FULL
 
    if(parity_done)
       next_state = DECODE_ADDRESS;
   else if(!parity_done && !low_pkt_valid)
       next_state = LOAD_DATA;
-  else(!parity_done && low_pkt_valid)
+  else if(!parity_done && low_pkt_valid)
       next_state = LOAD_PARITY;
-  
  end 
-
  endcase 
 end 
 
-// Output 
+// Output logic -- combinational 
+assign detect_add = (state == DECODE_ADDRESS);
+
+assign lfd_state = (state == LOAD_FIRST_DATA);
+
+assign  busy = (state == LOAD_FIRST_DATA) || (state == LOAD_PARITY)||
+(state == LOAD_AFTER_FULL)||(state == FIFO_FULL_STATE)||(state == LOAD_AFTER_FULL)
+|| (state == WAIT_TILL_EMPTY)||(state == CHECK_PARITY_ERROR);
+
+assign ld_state = (state == LOAD_DATA);
+
+assign write_en_reg = (state == LOAD_DATA) || (state == LOAD_PARITY)||
+(state == LOAD_AFTER_FULL);
+
+assign full_state = ( state == FIFO_FULL_STATE);
+
+assign laf_state = (state == LOAD_AFTER_FULL);
+
+assign low_pkt_valid = (state == LOAD_AFTER_FULL);
+
+assign rst_int_reg = (state == CHECK_PARITY_ERROR);
 
 endmodule 
