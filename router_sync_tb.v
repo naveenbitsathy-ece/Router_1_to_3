0602bca28ea6742975316b7d38745ef5a16df5e8
Naveen Raj <your_github_email@example.com>
2026-06-30 17:35:43 +0530

    new

--- router_sync_tb.v
+++ router_sync_tb.v
@@ -2,6 +2,7 @@
 
 module router_sync_tb;
 
+// Inputs
 reg clk;
 reg rst_n;
 reg detect_add;
@@ -17,6 +18,7 @@ reg full_0;
 reg full_1;
 reg full_2;
 
+// Outputs
 wire valid_out_0;
 wire valid_out_1;
 wire valid_out_2;
@@ -26,10 +28,7 @@ wire soft_rst_0;
 wire soft_rst_1;
 wire soft_rst_2;
 
-//-----------------------------------------
 // DUT
-//-----------------------------------------
-
 router_sync DUT(
     .clk(clk),
     .rst_n(rst_n),
@@ -55,33 +54,21 @@ router_sync DUT(
     .soft_rst_2(soft_rst_2)
 );
 
-//-----------------------------------------
 // Clock
-//-----------------------------------------
+initial
+    clk = 0;
 
-always #10 clk = ~clk;
+always
+    #10 clk = ~clk;
 
-//-----------------------------------------
-// Reset Task
-//-----------------------------------------
-
-task reset_dut;
+// Stimulus
+initial
 begin
+
+    // Reset
     rst_n = 0;
-    @(posedge clk);
-    @(posedge clk);
-    rst_n = 1;
-end
-endtask
-
-//-----------------------------------------
-// Initialize
-//-----------------------------------------
-
-task initialize;
-begin
     detect_add = 0;
-    data_in = 0;
+    data_in = 2'b00;
     write_en_reg = 0;
 
     read_en_0 = 0;
@@ -95,169 +82,124 @@ begin
     full_0 = 0;
     full_1 = 0;
     full_2 = 0;
-end
-endtask
 
-//-----------------------------------------
+    #20;
+    rst_n = 1;
+
+    //-----------------------------------
+    // Test FIFO0
+    //-----------------------------------
+    #20;
+    detect_add = 1;
+    data_in = 2'b00;
+    write_en_reg = 1;
+    full_0 = 1;
+
+    #20;
+    detect_add = 0;
+    full_0 = 0;
+
+    //-----------------------------------
+    // Test FIFO1
+    //-----------------------------------
+    #20;
+    detect_add = 1;
+    data_in = 2'b01;
+    full_1 = 1;
+
+    #20;
+    detect_add = 0;
+    full_1 = 0;
+
+    //-----------------------------------
+    // Test FIFO2
+    //-----------------------------------
+    #20;
+    detect_add = 1;
+    data_in = 2'b10;
+    full_2 = 1;
+
+    #20;
+    detect_add = 0;
+    full_2 = 0;
+
+    //-----------------------------------
+    // Check valid_out signals
+    //-----------------------------------
+    #20;
+    empty_0 = 0;
+    empty_1 = 0;
+    empty_2 = 0;
+
+    #20;
+    empty_0 = 1;
+    empty_1 = 1;
+    empty_2 = 1;
+
+    //-----------------------------------
+    // Soft Reset 0
+    //-----------------------------------
+    empty_0 = 0;
+
+    repeat(31)
+        @(posedge clk);
+
+    empty_0 = 1;
+
+    //-----------------------------------
+    // Soft Reset 1
+    //-----------------------------------
+    empty_1 = 0;
+
+    repeat(31)
+        @(posedge clk);
+
+    empty_1 = 1;
+
+    //-----------------------------------
+    // Soft Reset 2
+    //-----------------------------------
+    empty_2 = 0;
+
+    repeat(31)
+        @(posedge clk);
+
+    empty_2 = 1;
+
+    //-----------------------------------
+    // Read Enable Test
+    //-----------------------------------
+    empty_0 = 0;
+
+    repeat(10)
+        @(posedge clk);
+
+    read_en_0 = 1;
+
+    @(posedge clk);
+
+    read_en_0 = 0;
+
+    #100;
+
+    $finish;
+
+end
+
 // Monitor
-//-----------------------------------------
-
 initial
 begin
-$monitor("T=%0t Addr=%b Temp_Write=%b fifo_full=%b valid0=%b valid1=%b valid2=%b SR0=%b SR1=%b SR2=%b",
-$time,
-data_in,
-write_en,
-fifo_full,
-valid_out_0,
-valid_out_1,
-valid_out_2,
-soft_rst_0,
-soft_rst_1,
-soft_rst_2);
-end
-
-//-----------------------------------------
-// Test
-//-----------------------------------------
-
-initial
-begin
-
-clk = 0;
-
-initialize();
-reset_dut();
-
-
-//-----------------------------------------
-// Test FIFO0
-//-----------------------------------------
-
-@(posedge clk);
-detect_add = 1;
-data_in = 2'b00;
-write_en_reg = 1;
-
-@(posedge clk);
-detect_add = 0;
-
-full_0 = 1;
-
-#40;
-
-full_0 = 0;
-
-
-//-----------------------------------------
-// Test FIFO1
-//-----------------------------------------
-
-@(posedge clk);
-detect_add = 1;
-data_in = 2'b01;
-
-@(posedge clk);
-detect_add = 0;
-
-full_1 = 1;
-
-#40;
-
-full_1 = 0;
-
-
-//-----------------------------------------
-// Test FIFO2
-//-----------------------------------------
-
-@(posedge clk);
-detect_add = 1;
-data_in = 2'b10;
-
-@(posedge clk);
-detect_add = 0;
-
-full_2 = 1;
-
-#40;
-
-full_2 = 0;
-
-
-//-----------------------------------------
-// Test valid_out signals
-//-----------------------------------------
-
-empty_0 = 0;
-empty_1 = 0;
-empty_2 = 0;
-
-#40;
-
-empty_0 = 1;
-empty_1 = 1;
-empty_2 = 1;
-
-
-//-----------------------------------------
-// Soft Reset Test FIFO0
-//-----------------------------------------
-
-empty_0 = 0;
-
-repeat(31)
-@(posedge clk);
-
-empty_0 = 1;
-
-
-//-----------------------------------------
-// Soft Reset Test FIFO1
-//-----------------------------------------
-
-empty_1 = 0;
-
-repeat(31)
-@(posedge clk);
-
-empty_1 = 1;
-
-
-//-----------------------------------------
-// Soft Reset Test FIFO2
-//-----------------------------------------
-
-empty_2 = 0;
-
-repeat(31)
-@(posedge clk);
-
-empty_2 = 1;
-
-
-//-----------------------------------------
-// Counter Reset by Read Enable
-//-----------------------------------------
-
-empty_0 = 0;
-
-repeat(10)
-@(posedge clk);
-
-read_en_0 = 1;
-
-@(posedge clk);
-
-read_en_0 = 0;
-
-repeat(10)
-@(posedge clk);
-
-#100;
-
-$finish;
-
+    $monitor("Time=%0t Addr=%b write_en=%b fifo_full=%b valid0=%b valid1=%b valid2=%b SR0=%b SR1=%b SR2=%b",
+    $time,
+    data_in,
+    write_en,
+    fifo_full,
+    valid_out_0,
+    valid_out_1,
+    valid_out_2,
+    soft_rst_0,
+    soft_rst_1,
+    soft_rst_2);
 end
 
 endmodule
